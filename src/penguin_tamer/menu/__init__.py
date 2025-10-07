e4b8740b764626665f616c6e01e8a3b4768d89f4
Andrey BCH <andrey.bch.1976@gmail.com>
2025-10-07 21:00:18 +0300

    Очистка и декомпозиция

--- src/penguin_tamer/menu/__init__.py
+++ src/penguin_tamer/menu/__init__.py
@@ -1,4 +1,4 @@
 # Menu package for penguin-tamer
-from .config_menu import main_menu
+from .config_menu import main_menu, ConfigMenuApp
 
-__all__ = ["main_menu"]
+__all__ = ["main_menu", "ConfigMenuApp"]
